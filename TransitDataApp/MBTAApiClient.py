79ed6bc15a7a8aad61b657c4ceed4d66514f1e5a
Nicolas Pedraza Bolivar <npedraza092002@gmail.com>
2025-02-26 06:12:24 -0800

    Add files via upload

---
+++ TransitDataApp/MBTAApiClient.py
@@ -0,0 +1,25 @@
+import urllib.request, json
+import mysqldb
+
+def callMBTAApi():
+    mbtaDictList = []
+    mbtaUrl = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip'
+    with urllib.request.urlopen(mbtaUrl) as url:
+        data = json.loads(url.read().decode())
+        for bus in data['data']:
+            busDict = dict()
+            # complete the fields below based on the entries of your SQL table
+            busDict['route_number'] = 1
+            busDict['id'] = bus['id']
+            busDict['latitude'] = bus['attributes']['latitude']
+            busDict['longitude'] = bus['attributes']['longitude']
+            busDict['bearing'] = bus['attributes']['bearing']
+            busDict['current_status'] = bus['attributes']['current_status']
+            busDict['current_stop_sequence'] = bus['attributes']['current_stop_sequence']
+            busDict['direction_id'] = bus['attributes']['direction_id']
+            busDict['occupancy_status'] = bus['attributes']['occupancy_status']
+            busDict['updated_at'] = bus['attributes']['updated_at']
+            mbtaDictList.append(busDict)
+    mysqldb.insertMBTARecord(mbtaDictList) 
+
+    return mbtaDictList  
